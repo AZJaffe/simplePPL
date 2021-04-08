c4cff695dd6bca6e0bd59f032002a20b0cc87117
Adam Jaffe <azjaffe1@gmail.com>
2021-04-08 17:33:43 -0400

    Add more distributions and tests for them

--- simplePPL.py
+++ simplePPL.py
@@ -18,11 +18,22 @@ def check_arity(dist, nargs):
   dists_to_arity = {
     'Bern': 1,
     'N': 2,
-    'Unif': 2
+    'Unif': 2,
+    'Beta': 2,
+    'Pois': 1,
+    'DUnif': 2,
+    'Dir': 1,
+    'Gamma': 2,
+    'Exp': 1,
+    'Geometric': 1,
+    'Binom': 2,
   }
+  print(f'checking arity of {dist} given {nargs}')
   if dist not in dists_to_arity:
+    print('Undefined Dist')
     raise UndefinedDistribution(dist)
   if dists_to_arity[dist] != nargs:
+    print('Wrong Arity')
     raise WrongArity(dist, nargs, dists_to_arity[dist])
 
 def get_num_args(arglist):
@@ -54,12 +65,31 @@ def distributed_stmt(m, store, stmt):
   arglist = dist_stmt.children[1]
   args = process_arglist(store, arglist)
   check_arity(dist, len(args))
+  # Discrete
   if dist == 'Bern':
     store[var] = m.Var(var, pm.Bernoulli.dist(p=args[0], testval=0.5))
+  elif dist == 'Unif':
+    store[var] = m.Var(var, pm.Uniform.dist(lower=args[0], upper=args[1], testval=args[0]))
+  elif dist == 'Beta':
+    store[var] = m.Var(var, pm.Beta.dist(alpha=args[0], beta=args[1], testval=0))
+  elif dist == 'Pois':
+    store[var] = m.Var(var, pm.Poisson.dist(mu=args[0], testval=0))
+  elif dist == 'DUnif':
+    store[var] = m.Var(var, pm.DiscreteUniform.dist(lower=args[0], upper=args[1], testval=0))
+  elif dist == 'Binom':
+    store[var] = m.Var(var, pm.Binomial.dist(n=args[0], p=args[1], testval=0))
+  elif dist == 'Geometric':
+    store[var] = m.Var(var, pm.Geometric.dist(p=args[0], testval=0))
+  # Continuous
   elif dist == 'N':
     store[var] = m.Var(var, pm.Normal.dist(mu=args[0], sigma=args[1],testval=0))
-  elif dist == 'Unif':
-    store[var] = m.Var(var, pm.Normal.dist(lower=args[0], upper=args[1]))
+  # elif dist == 'Dir':
+  #   store[var] = m.Var(var, pm.Dirchlet.dist(a=args[0]))
+  elif dist == 'Exp':
+    store[var] = m.Var(var, pm.Exponential.dist(lam=args[0], testval=0))
+  # Multivariate
+  elif dist == 'Gamma':
+    store[var] = m.Var(var, pm.Gamma.dist(alpha=args[0], beta=args[1], testval=0))
 
 def lookup(store, key):
   if key not in store:
--- test/test.py
+++ test/test.py
@@ -9,3 +9,44 @@ s = simplePPL.run(p)
 assert str(s['z']) == 'z ~ Bernoulli'
 assert str(s['x1']) == 'x1 ~ Normal'
 assert str(s['x2']) == 'x2 ~ Normal'
+
+s = simplePPL.run('x ~ Unif(0,1)')
+assert str(s['x']) == 'x ~ Uniform'
+s = simplePPL.run('x ~ Bern(0.5)')
+assert str(s['x']) == 'x ~ Bernoulli'
+s = simplePPL.run('x ~ N(0,1)')
+assert str(s['x']) == 'x ~ Normal'
+s = simplePPL.run('x ~ Beta(5,2)')
+assert str(s['x']) == 'x ~ Beta'
+s = simplePPL.run('x ~ Pois(5)')
+assert str(s['x']) == 'x ~ Poisson'
+s = simplePPL.run('x ~ DUnif(0,1)')
+assert str(s['x']) == 'x ~ DiscreteUniform'
+s = simplePPL.run('x ~ DUnif(0,1)')
+assert str(s['x']) == 'x ~ DiscreteUniform'
+s = simplePPL.run('x ~ Binom(5,0.5)')
+assert str(s['x']) == 'x ~ Binomial'
+s = simplePPL.run('x ~ Geometric(0.5)')
+assert str(s['x']) == 'x ~ Geometric'
+s = simplePPL.run('x ~ Exp(5)')
+assert str(s['x']) == 'x ~ Exponential'
+s = simplePPL.run('x ~ Gamma(5,2)')
+assert str(s['x']) == 'x ~ Gamma'
+
+try:
+  s = simplePPL.run('x ~ NotADistribution(5,2)')
+  assert False
+except simplePPL.UndefinedDistribution:
+  print('Good job')
+
+try:
+  s = simplePPL.run('x ~ Bern()')
+  assert False
+except simplePPL.WrongArity:
+  print('Good job')
+
+try:
+  s = simplePPL.run('x ~ Bern(0.1, 0.2)')
+  assert False
+except simplePPL.WrongArity:
+  pass
