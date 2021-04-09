e4d81c5b42612f526c329eec25096816a64b08a0
Adam Jaffe <azjaffe1@gmail.com>
2021-04-09 00:06:54 -0400

    wip adding support for data literals

--- simplePPL.py
+++ simplePPL.py
@@ -1,10 +1,15 @@
 from lark import Lark
 import sys
 import pymc3 as pm
+import numpy as np
 
 class UnitializedVariable(Exception):
   def __init__(self, var):
     super().__init__(f'Random variable {var} used before declared')
+
+class DuplicateVariable(Exception):
+  def __init__(self, var):
+    super().__init__(f'Random variable {var} declared twice')
 
 class WrongArity(Exception):
   def __init__(self, dist, actual, expected):
@@ -13,6 +18,39 @@ class WrongArity(Exception):
 class UndefinedDistribution(Exception):
   def __init__(self, dist):
     super().__init__(f'Unknown distribution {dist}')
+
+class InvalidDataLiteral(Exception):
+  def __init__(self, var=''):
+    super().__init__()
+
+  def set_var(self, var):
+    self.var = var
+
+  def __str__(self):
+    if self.var == '':
+      return f'Invalid data literal'
+    else:
+      return f'Invalid data literal for variable {var}'
+
+class Store():
+  def __init__(self):
+    self.rvs = {}
+    self.data = {}
+
+  def add_rv(self, var, rv):
+    if var in self.rvs:
+      raise DuplicateVariable(var)
+    self.rvs[var] = rv
+
+  def add_data(self, var, data):
+    if var in self.rvs:
+      raise DuplicateVariable(var)
+    self.data[var] = data
+
+  def lookup_rv(self, var):
+    if var not in self.rvs:
+      raise UnitializedVariable(var)
+    return self.rvs[var]  
 
 def check_arity(dist, nargs):
   dists_to_arity = {
@@ -38,11 +76,13 @@ def run(program):
   p = parser.parse(program)
   print(p)
   print(p.pretty())
-  store = {}
+  store = Store()
   m = pm.Model()
   for stmt in p.children:
     if stmt.data == 'distributed':
       distributed_stmt(m, store, stmt)
+    if stmt.data == 'dataassign':
+      data_assign_stmt(store, stmt)
   return store
 
 def distributed_stmt(m, store, stmt):
@@ -53,34 +93,52 @@ def distributed_stmt(m, store, stmt):
   check_arity(dist, len(args))
   # Discrete
   if dist == 'Bern':
-    store[var] = m.Var(var, pm.Bernoulli.dist(p=args[0], testval=0.5))
+    store.add_rv(var, m.Var(var, pm.Bernoulli.dist(p=args[0], testval=0.5)))
   elif dist == 'Unif':
-    store[var] = m.Var(var, pm.Uniform.dist(lower=args[0], upper=args[1], testval=args[0]))
+    store.add_rv(var, m.Var(var, pm.Uniform.dist(lower=args[0], upper=args[1], testval=args[0])))
   elif dist == 'Beta':
-    store[var] = m.Var(var, pm.Beta.dist(alpha=args[0], beta=args[1], testval=0))
+    store.add_rv(var, m.Var(var, pm.Beta.dist(alpha=args[0], beta=args[1], testval=0)))
   elif dist == 'Pois':
-    store[var] = m.Var(var, pm.Poisson.dist(mu=args[0], testval=0))
+    store.add_rv(var, m.Var(var, pm.Poisson.dist(mu=args[0], testval=0)))
   elif dist == 'DUnif':
-    store[var] = m.Var(var, pm.DiscreteUniform.dist(lower=args[0], upper=args[1], testval=0))
+    store.add_rv(var, m.Var(var, pm.DiscreteUniform.dist(lower=args[0], upper=args[1], testval=0)))
   elif dist == 'Binom':
-    store[var] = m.Var(var, pm.Binomial.dist(n=args[0], p=args[1], testval=0))
+    store.add_rv(var, m.Var(var, pm.Binomial.dist(n=args[0], p=args[1], testval=0)))
   elif dist == 'Geometric':
-    store[var] = m.Var(var, pm.Geometric.dist(p=args[0], testval=0))
+    store.add_rv(var, m.Var(var, pm.Geometric.dist(p=args[0], testval=0)))
   # Continuous
   elif dist == 'N':
-    store[var] = m.Var(var, pm.Normal.dist(mu=args[0], sigma=args[1],testval=0))
+    store.add_rv(var, m.Var(var, pm.Normal.dist(mu=args[0], sigma=args[1],testval=0)))
   # elif dist == 'Dir':
-  #   store[var] = m.Var(var, pm.Dirchlet.dist(a=args[0]))
+  #   store.add_rv(var, m.Var(var, pm.Dirchlet.dist(a=args[0])))
   elif dist == 'Exp':
-    store[var] = m.Var(var, pm.Exponential.dist(lam=args[0], testval=0))
+    store.add_rv(var, m.Var(var, pm.Exponential.dist(lam=args[0], testval=0)))
   # Multivariate
   elif dist == 'Gamma':
-    store[var] = m.Var(var, pm.Gamma.dist(alpha=args[0], beta=args[1], testval=0))
+    store.add_rv(var, m.Var(var, pm.Gamma.dist(alpha=args[0], beta=args[1], testval=0)))
 
-def lookup(store, key):
-  if key not in store:
-    raise UnitializedVariable(key)
-  return store[key]
+def data_assign_stmt(store, stmt):
+  var = stmt.children[0].children[0].value
+  data = stmt.children[1]
+  a = parse_data(data)
+  store.add_data(var, data)
+
+def parse_data(data):
+  if len(data.children) == 0:
+    return np.array([])
+  if data.children[0].data == 'number':
+    a = np.zeros((len(data.children)))
+    for i,child in enumerate(data.children):
+      if child.data != 'number':
+        raise InvalidDataLiteral()
+      a[i] = float(child.children[0])
+    return a
+  else:
+    a = [parse_data(child) for child in data.children]
+    for subarray in a:
+      if a[0].shape != subarray.shape:
+        raise InvalidDataLiteral()
+    return np.stack(a)
 
 def process_numexpr(store, numexp):
   # Base Cases
--- test/test.py
+++ test/test.py
@@ -1,50 +1,76 @@
 import simplePPL
+import unittest
+import numpy as np
+from lark import Lark
 
-p = simplePPL.load('./examples/bern.ppl')
-s = simplePPL.run(p)
-assert str(s['x']) == 'x ~ Bernoulli'
+class Test(unittest.TestCase):
 
-p = simplePPL.load('./examples/mixture_of_gaussians.ppl')
-s = simplePPL.run(p)
-assert str(s['z']) == 'z ~ Bernoulli'
-assert str(s['x1']) == 'x1 ~ Normal'
-assert str(s['x2']) == 'x2 ~ Normal'
+  def setUp(self):
+    self.parser = Lark.open('./grammar.lark', start='simpleppl')
 
-s = simplePPL.run('x ~ Unif(0,1)')
-assert str(s['x']) == 'x ~ Uniform'
-s = simplePPL.run('x ~ Bern(0.5)')
-assert str(s['x']) == 'x ~ Bernoulli'
-s = simplePPL.run('x ~ N(0,1)')
-assert str(s['x']) == 'x ~ Normal'
-s = simplePPL.run('x ~ Beta(5,2)')
-assert str(s['x']) == 'x ~ Beta'
-s = simplePPL.run('x ~ Pois(5)')
-assert str(s['x']) == 'x ~ Poisson'
-s = simplePPL.run('x ~ DUnif(0,1)')
-assert str(s['x']) == 'x ~ DiscreteUniform'
-s = simplePPL.run('x ~ Binom(5,0.5)')
-assert str(s['x']) == 'x ~ Binomial'
-s = simplePPL.run('x ~ Geometric(0.5)')
-assert str(s['x']) == 'x ~ Geometric'
-s = simplePPL.run('x ~ Exp(5)')
-assert str(s['x']) == 'x ~ Exponential'
-s = simplePPL.run('x ~ Gamma(5,2)')
-assert str(s['x']) == 'x ~ Gamma'
+  def test_distributions(self):
+    s = simplePPL.run('x ~ Unif(0,1)')
+    self.assertEqual(str(s.lookup_rv('x')), 'x ~ Uniform')
+    s = simplePPL.run('x ~ Bern(0.5)')
+    self.assertEqual(str(s.lookup_rv('x')), 'x ~ Bernoulli')
+    s = simplePPL.run('x ~ N(0,1)')
+    self.assertEqual(str(s.lookup_rv('x')), 'x ~ Normal')
+    s = simplePPL.run('x ~ Beta(5,2)')
+    self.assertEqual(str(s.lookup_rv('x')), 'x ~ Beta')
+    s = simplePPL.run('x ~ Pois(5)')
+    self.assertEqual(str(s.lookup_rv('x')), 'x ~ Poisson')
+    s = simplePPL.run('x ~ DUnif(0,1)')
+    self.assertEqual(str(s.lookup_rv('x')), 'x ~ DiscreteUniform')
+    s = simplePPL.run('x ~ Binom(5,0.5)')
+    self.assertEqual(str(s.lookup_rv('x')), 'x ~ Binomial')
+    s = simplePPL.run('x ~ Geometric(0.5)')
+    self.assertEqual(str(s.lookup_rv('x')), 'x ~ Geometric')
+    s = simplePPL.run('x ~ Exp(5)')
+    self.assertEqual(str(s.lookup_rv('x')), 'x ~ Exponential')
+    s = simplePPL.run('x ~ Gamma(5,2)')
+    self.assertEqual(str(s.lookup_rv('x')), 'x ~ Gamma')
 
-try:
-  s = simplePPL.run('x ~ NotADistribution(5,2)')
-  assert False
-except simplePPL.UndefinedDistribution:
-  print('Good job')
+  def test_examples(self):
+    p = simplePPL.load('./examples/bern.ppl')
+    s = simplePPL.run(p)
+    self.assertEqual(str(s.lookup_rv('x')), 'x ~ Bernoulli')
 
-try:
-  s = simplePPL.run('x ~ Bern()')
-  assert False
-except simplePPL.WrongArity:
-  print('Good job')
+    p = simplePPL.load('./examples/mixture_of_gaussians.ppl')
+    s = simplePPL.run(p)
+    self.assertEqual(str(s.lookup_rv('z')), 'z ~ Bernoulli')
+    self.assertEqual(str(s.lookup_rv('x1')), 'x1 ~ Normal')
+    self.assertEqual(str(s.lookup_rv('x2')), 'x2 ~ Normal')
 
-try:
-  s = simplePPL.run('x ~ Bern(0.1, 0.2)')
-  assert False
-except simplePPL.WrongArity:
-  pass
+  def test_distribution_errors(self):
+    try:
+      s = simplePPL.run('x ~ NotADistribution(5,2)')
+      self.assertTrue(False)
+    except simplePPL.UndefinedDistribution:
+      pass
+    try:
+      s = simplePPL.run('x ~ Bern()')
+      self.assertTrue(False)
+    except simplePPL.WrongArity:
+      pass
+    try:
+      s = simplePPL.run('x ~ Bern(0.1, 0.2)')
+      self.assertTrue(False)
+    except simplePPL.WrongArity:
+      pass
+
+  def test_data_parse(self):
+    p = self.parser.parse('x = [1,2,3]')
+    d = p.children[0].children[1]
+    a = simplePPL.parse_data(d)
+    self.assertTrue((np.array([1,2,3]) == a).all())
+
+    p = self.parser.parse('x = [1,[2],3]')
+    d = p.children[0].children[1]
+    try:
+      a = simplePPL.parse_data(d)
+      self.assertTrue(False)
+    except simplePPL.InvalidDataLiteral:
+      pass
+
+if __name__ == '__main__':
+    unittest.main()
